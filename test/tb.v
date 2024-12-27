f93c39ce474a03c07a0addbd157c91a9ad683737
Moldir Azhimukhanbet <azhimukhanbetmoldir@gmail.com>
2024-12-27 14:01:12 +0000

    Updated README

--- test/tb.v
+++ test/tb.v
@@ -6,29 +6,34 @@
 */
 module tb ();
 
-  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
+  // Dump the signals to a VCD file. You can view it with gtkwave.
   initial begin
     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
-    #1;
+    //#1;
   end
 
   // Wire up the inputs and outputs:
   reg clk;
   reg rst_n;
   reg ena;
-  reg [7:0] ui_in;
   reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+  reg [4:0] ui_in_7to3;
+  reg [1:0] ui_in_2to1;
+  reg ui_in_0;
+
 `ifdef GL_TEST
   wire VPWR = 1'b1;
   wire VGND = 1'b0;
 `endif
 
+  wire [6:0] uo_out_7to1;
+  wire uo_out_0;
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
+
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_bitty user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
@@ -36,9 +41,9 @@ module tb ();
       .VGND(VGND),
 `endif
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
+      .uio_in  (uio_in),    // Dedicated inputs
+      .uo_out ({uo_out_7to1, uo_out_0}),   // Dedicated outputs
+      .ui_in ({ui_in_7to3, ui_in_2to1, ui_in_0}),   // IOs: Input path
       .uio_out(uio_out),  // IOs: Output path
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
       .ena    (ena),      // enable - goes high when design is selected
